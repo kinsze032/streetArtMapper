736f6fd26f341e0edb2c23e86d02bb7a765cdc76
kinsze032 <szedzielarzkinga@gmail.com>
2023-04-30 08:48:36 +0200

    strona główna

--- artbase/models.py
+++ artbase/models.py
@@ -1,39 +1,40 @@
 from django.db import models
 
 
-CATEGORIES = (
-    (1, "mural"),
-    (2, "graffiti"),
-    (3, "instalacja"),
-    (4, "neon"),
-)   # jako model zrobić.
-
-
 class StreetArt(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
     description = models.TextField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    location = models.ForeignKey('Location', on_delete=models.CASCADE) # oneToOne
+    location = models.OneToOneField('Location', on_delete=models.CASCADE)
     # rated_by_users = models.ManyToManyField(User, through='StreetArtUserRating')
     #można się odwołać do listy Street Artu ocenionego przez użytkownika
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.category}"
 
 
 class Category(models.Model):
-    name = models.IntegerField(choices=CATEGORIES)
+    class ArtworkType(models.IntegerChoices):
+        MURAL = 1, 'mural'
+        GRAFFITI = 2, 'graffiti'
+        INSTALACJA = 3, 'instalacja'
+        NEON = 4, 'neon'
+
+    type = models.IntegerField(choices=ArtworkType.choices)
+
+    def __str__(self):
+        return self.get_type_display()
 
 
 class Location(models.Model):
     city = models.CharField(max_length=100)
     longitude = models.FloatField()
-    latitude = models.DecimalField(max_digits=8, decimal_places=6)
+    latitude = models.FloatField()
 
     def __str__(self):
-        return f"{self.city}"
+        return self.city
 
 
 # class StreetArtUserRating(models.Model):
--- artbase/views.py
+++ artbase/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render
 from django.views import View
+import folium
+from folium.plugins import FastMarkerCluster
+
+from artbase.models import StreetArt, Category, Location
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "artbase/base.html")
+        arts = StreetArt.objects.all()
+
+        #create a Folium map centered on Katowice
+        map = folium.Map(location=[50.26480752, 19.02348142], zoom_start=12)
+
+        # # add a marker to the map for each street art
+        for art in arts:
+            coordinates = (art.location.latitude, art.location.longitude)
+            popup_html = f"<a href='/streetart/{art.id}'>{art.title} ({art.category.get_type_display()})</a>"
+
+            if art.category.get_type_display() == "mural":
+                folium.Marker(
+                    coordinates,
+                    popup=popup_html,
+                    icon=folium.Icon(color="red", icon="info-sign")
+                ).add_to(map)
+            if art.category.get_type_display() == "neon":
+                folium.Marker(
+                    coordinates,
+                    popup=popup_html,
+                    icon=folium.Icon(color="blue", icon="info-sign")
+                ).add_to(map)
+            if art.category.get_type_display() == "graffiti":
+                folium.Marker(
+                    coordinates,
+                    popup=popup_html,
+                    icon=folium.Icon(color="green", icon="info-sign")
+                ).add_to(map)
+            if art.category.get_type_display() == "instalacja":
+                folium.Marker(
+                    coordinates,
+                    popup=popup_html,
+                    icon=folium.Icon(color="darkpurple", icon="info-sign")
+                ).add_to(map)
+
+        #use FastMarerCluster to generate the clusters on the map
+        #to do this, we pass list of all(lat, lon) tuples in the data
+        # latitudes = [art.location.latitude for art in arts]
+        # longitudes = [art.location.longitude for art in arts]
+        #
+        # FastMarkerCluster(data=list(zip(latitudes, longitudes))).add_to(map)
+
+        context = {'map': map._repr_html_()}
+        return render(request, "artbase/base.html", context)
+
